4a1cdf6aea4c53c5da59a6ccee7624c3052e331a
Aaron Bolyard <adb@aaronbolyard.com>
2018-02-27 18:03:32 -0500

    First assignment.

---
+++ P1T1_BolyardAaron.py
@@ -0,0 +1,13 @@
+# P1T1
+# Different take on Hello World, with a prompt
+# Aaron Bolyard
+# 2018-02-06
+
+print ("Hello, I'm Aaron Bolyard.")
+print ("The classmate in front of me is Steve Nyhuis.")
+print ("And the student to my right is George Ulrich.")
+print ("The program was verified by Candice Kirk.")
+
+print ("So what's your name?")
+name = input ()
+print ("Why hello, {}!".format (name))
